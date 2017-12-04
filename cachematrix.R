2fcc986e7cce4cbe20ee48e2938e77d8b696598f
ENI - Mark Canete <mark.canete@ENERGYNET.POWEREDGE.local>
2017-12-04 12:23:07 +0800

    Revert "Update cachematrix.R"
    This reverts commit ce3f2e3e8a0e38ce9d68b9c714f743db61338eca.

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,15 @@
-# This will Creates a matrix that can cache it's inverse, if the inverse has already been
-#calculated before, the Cached inverse is return
-# 
-#
-# Returns:
-# A matrix with functions to get/set value & get_matrix/set_matrix inverse
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
-        inverse_math <- NULL
-       
-        ##function get_matrix and set_matrix for matrix 
-       get_matrix <- function() x
-          set_matrix <- function(y){
-            x <<- y
-          inverse_math <<- NULL
-  ## function getinv for inverse of matrix                
-  getinv <- function() inverse_math
-  setinv <- function(inverse) inverse_math <<- inverse
-  
-  ## return list of function for matrix
-    list(get_matrix=get_matrix, set_matrix = set_matrix, getinv=getinv,setinv=setinv)
 }
 
 
-## Computes the inverse of a matrix
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    inverse_math <- x$getinv()
-  
-  if(!is.null(inverse_math)){
-    message("inverse is cached")
-    return(inverse_math)
-  }
-  m <- x$get_matrix()
-  inverse_math <- solve(m,...)
-  
-  x$setinv(inverse_math)
-  
-  return(inverse_math)
-}     
-        
 }
